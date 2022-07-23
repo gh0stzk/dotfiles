ae9cf09b743dd276afc74d1eb1b8736ec580dcba
gh0stzk <67278339+gh0stzk@users.noreply.github.com>
2022-07-23 17:24:50 -0500

    Add files via upload

---
+++ .config/bspwm/scripts/updates.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#source https://github.com/x70b1/polybar-scripts
+
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+    echo "$updates"
+else
+    echo "0"
+fi
