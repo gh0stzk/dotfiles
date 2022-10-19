2ee02e624abd088c05abd56677f5951e9bacb957
gh0stzk <67278339+gh0stzk@users.noreply.github.com>
2022-10-19 08:59:03 -0500

    uploaded firefox theme

---
+++ misc/firefox/user.js
@@ -0,0 +1,28 @@
+// ** Theme Related Options ****************************************************
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+user_pref("browser.cache.memory.capacity", 524288);
+user_pref("browser.sessionstore.interval", 15000000);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabledFirstsession", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.server", 0);
+user_pref("extensions.pocket.enabled", false);
+user_pref("reader.parse-on-load.enabled", false);
+
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
