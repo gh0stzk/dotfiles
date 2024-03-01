4259fb7bdd4033b7e58f593d4379667a581854ae
gh0stzk <z0mbi3.zk@protonmail.com>
2024-03-01 13:45:57 -0600

    beautysh is deprecated. Change to shfmt, bash formater

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,7 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.shfmt,
         -- null_ls.builtins.code_actions.shellcheck,
         -- null_ls.builtins.diagnostics.erb_lint,
         -- null_ls.builtins.diagnostics.eslint_d,
