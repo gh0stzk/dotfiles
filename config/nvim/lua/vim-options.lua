16e5bcbc2e67012eb80293813aab2ff879f9649c
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2024-04-09 11:16:50 +0200

    Fix : Translated lines in vim-options.lua

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -1,7 +1,50 @@
 local opt = vim.opt -- for conciseness
 
--- Numeracion de las lineas
+
+-- Line numbering
 opt.relativenumber = true
+opt.number = true
+opt.showtabline = 2
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+
+-- Line wrapping
+opt.wrap = true
+
+
+-- Search setting
+opt.ignorecase = true
+opt.smartcase = true
+
+
+-- Cursor line
+opt.cursorline = true
+
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+
+-- Backspace
+opt.backspace = "indent,eol,start"
+
+
+-- Clipboard
+opt.clipboard:append("unnamedplus")
+
+
+-- Splitt Windows
+opt.splitright = true
+opt.splitbelow = truelocal opt = vim.opt -- for conciseness
+
+-- Numeracion de las lineas
+opt.relativenumber = tru
 opt.number = true
 opt.showtabline = 2
 -- tabs & indentation
@@ -31,7 +74,7 @@ opt.backspace = "indent,eol,start"
 -- Clipboard
 opt.clipboard:append("unnamedplus")
 
--- Splitt Windows
+-- Split Windows
 opt.splitright = true
 opt.splitbelow = true
 
