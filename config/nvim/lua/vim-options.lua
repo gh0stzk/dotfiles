1102469b1ead18b2eb7f61fb67744132130cccfb
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2024-04-12 20:00:38 +0000

    Fix : Duplicated lines in file and removed useless blank lines

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -1,81 +1,39 @@
 local opt = vim.opt -- for conciseness
 
-
 -- Line numbering
 opt.relativenumber = true
 opt.number = true
 opt.showtabline = 2
+
 -- tabs & indentation
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
-
 -- Line wrapping
 opt.wrap = true
-
 
 -- Search setting
 opt.ignorecase = true
 opt.smartcase = true
 
-
 -- Cursor line
 opt.cursorline = true
-
 
 -- appearance
 opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
 
-
 -- Backspace
 opt.backspace = "indent,eol,start"
 
-
 -- Clipboard
 opt.clipboard:append("unnamedplus")
-
 
 -- Splitt Windows
 opt.splitright = true
 opt.splitbelow = truelocal opt = vim.opt -- for conciseness
-
--- Numeracion de las lineas
-opt.relativenumber = tru
-opt.number = true
-opt.showtabline = 2
--- tabs & indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.autoindent = true
-
--- Line wrapping
-opt.wrap = true
-
--- Search setting
-opt.ignorecase = true
-opt.smartcase = true
-
--- Cursor line
-opt.cursorline = true
-
--- appearance
-opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
-
--- Backspace
-opt.backspace = "indent,eol,start"
-
--- Clipboard
-opt.clipboard:append("unnamedplus")
-
--- Split Windows
-opt.splitright = true
-opt.splitbelow = true
 
 opt.iskeyword:append("-")
