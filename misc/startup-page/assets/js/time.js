671ec047f7053eab2e2c377b9ee7f0be816681d5
Allan Alvarez Ortiz <flick36@gmail.com>
2024-04-21 04:10:33 -0600

    Update time.js

--- misc/startup-page/assets/js/time.js
+++ misc/startup-page/assets/js/time.js
@@ -5,10 +5,10 @@
 
 const displayClock = () => {
   const options = {day:'numeric', month:'short', hour: 'numeric', minute: 'numeric', hour12: CONFIG.twelveHourFormat, timeZone: CONFIG.timeZone}
-	const date = new Date().toLocaleString("en-US", options).split(/[\s,]+/)
+  const date = new Date().toLocaleString("en-US", options).split(/[\s,]+/)
   
-	document.getElementById('date').innerText = `${date[0]} ${date[1]}`
-	document.getElementById('time').innerText = `${date[2]} ${CONFIG.twelveHourFormat ? date[3] : ''}`
+  document.getElementById('date').innerText = `${date[0]} ${date[1]}`
+  document.getElementById('time').innerText = `${date[2]} ${CONFIG.twelveHourFormat ? date[3] : ''}`
 
   setTimeout(displayClock, 1000)
 }
