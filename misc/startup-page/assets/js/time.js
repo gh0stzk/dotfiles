e4658ca7ed979c0224d81aa361218c31aa4d2cde
Allan Alvarez Ortiz <flick36@gmail.com>
2024-04-21 04:17:47 -0600

    Update time.js

--- misc/startup-page/assets/js/time.js
+++ misc/startup-page/assets/js/time.js
@@ -8,7 +8,7 @@ const displayClock = () => {
   const date = new Date().toLocaleString("en-US", options).split(/[\s,]+/)
   
   document.getElementById('date').innerText = `${date[0]} ${date[1]}`
-  document.getElementById('time').innerText = `${date[2]} ${CONFIG.twelveHourFormat ? date[3] : ''}`
+  document.getElementById('time').innerText = `${date[2]} ${CONFIG.twelveHourFormat ? ` ${date[3]}` : ''}`
 
   setTimeout(displayClock, 1000)
 }
