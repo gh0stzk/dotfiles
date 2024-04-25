059be17eff25007b207bc105fbb540e6479b2266
adilhyz <sandyadilhayyi@gmail.com>
2024-04-26 04:54:56 +0700

    fix temperature script

--- config/bspwm/scripts/temp.sh
+++ config/bspwm/scripts/temp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-TEMP="$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon0/temp2_input | awk '{sub(/000$/, "°C", $0); print}')"
+TEMP="$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp2_input | awk '{sub(/000$/, "°C", $0); print}')"
 echo "${TEMP:-null}"
