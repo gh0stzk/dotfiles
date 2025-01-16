6de8549f05dd6531119bedb05174660190b0d2fa
ilshatvm <ilshatvm13@gmail.com>
2025-01-17 04:21:32 +0500

    fix dual monitor support for Andrea and z0mbi3 themes

--- config/bspwm/rices/andrea/Bar.bash
+++ config/bspwm/rices/andrea/Bar.bash
@@ -1,5 +1,38 @@
 # This file launch the bar/s
-eww -c "${HOME}"/.config/bspwm/rices/"${RICE}"/andy open --toggle bar
+
+# Function for generating workspaces.yuck file with eww widgets
+generate_eww_workspaces() {
+	local eww_file="${HOME}/.config/bspwm/rices/${RICE}/andy/workspaces.yuck"
+	local monitors=$(bspc query -M --names)
+	local count=0
+	local listen_workspaces=""
+	local widgets=""
+	local workspace_widgets=";; Workspaces Widgets ;;\n"
+
+	echo ";; Workspaces ;;" > "$eww_file"
+
+	for monitor in $monitors; do
+		local workspace_name="workspace${count}"
+		listen_workspaces+="(deflisten ${workspace_name} \"scripts/WorkSpaces ${monitor}\")\n"
+		widgets+="         (box :visible { monitor==\"${monitor}\" } (${workspace_name}))\n"
+		workspace_widgets+="(defwidget ${workspace_name} [] (literal :content ${workspace_name}))\n"
+		count=$((count + 1))
+	done
+
+	echo -e "$listen_workspaces" >> "$eww_file"
+	echo -e "$workspace_widgets" >> "$eww_file"
+	echo -e ";; Workspaces Main Widget ;;\n(defwidget workspaces [monitor]\n    (box :halign \"center\"\n         :spacing 15\n         :space-evenly \"false\"\n$widgets))" >> "$eww_file"
+}
+
+generate_eww_workspaces
+
+# Get a list of monitors and sort them so that the primary monitor is first
+local monitors=$(xrandr -q | grep -w 'connected' | sort -k3n | cut -d' ' -f1)
+local count=0
+for m in $monitors; do
+	eww -c "${HOME}"/.config/bspwm/rices/"${RICE}"/andy open bar --id "$m" --arg monitor="$m" --toggle --screen $count
+	count=$((count + 1))
+done
 
 	# Fix eww,  when entering fullscreen state
 	bspc subscribe node_state | while read -r _ _ _ _ state flag; do
--- config/bspwm/rices/z0mbi3/Bar.bash
+++ config/bspwm/rices/z0mbi3/Bar.bash
@@ -1,5 +1,37 @@
 # This file launch the bar/s
-eww -c "${HOME}"/.config/bspwm/rices/"${RICE}"/bar open --toggle bar
+
+# Function for generating workspaces.yuck file with eww widgets
+generate_eww_workspaces() {
+    local eww_file="${HOME}/.config/bspwm/rices/${RICE}/bar/workspaces.yuck"
+    local monitors=$(bspc query -M --names)
+    local count=0
+    local listen_workspaces=""
+    local widgets=""
+    local workspace_widgets=";; Workspaces Widgets ;;\n"
+
+    echo ";; Workspaces ;;" > "$eww_file"
+
+    for m in $monitors; do
+			local workspace_name="workspace${count}"
+            listen_workspaces+="(deflisten ${workspace_name} \"scripts/WorkSpaces $m\")\n"
+            widgets+="           (box :visible { monitor==\""$m\"" } (${workspace_name}))\n"
+            workspace_widgets+="(defwidget ${workspace_name} [] (literal :content ${workspace_name}))\n"
+            count=$((count + 1))
+ 	done
+    echo -e $listen_workspaces >> "$eww_file"
+    echo -e $workspace_widgets >> "$eww_file"
+    echo -e ";; Workspaces Main Widget ;;\n(defwidget workspaces [monitor]\n   (box    :orientation \"v\"\n           :space-evenly \"false\"\n           :valign \"start\"\n$widgets))" >> "$eww_file"
+}
+
+generate_eww_workspaces
+
+# Get a list of monitors and sort them so that the primary monitor is first
+local monitors=$(xrandr -q | grep -w 'connected' | sort -k3n | cut -d' ' -f1)
+local count=0
+for m in $monitors; do
+	eww -c "${HOME}"/.config/bspwm/rices/"${RICE}"/bar open bar --id "$m" --arg monitor="$m" --toggle --screen $count
+    count=$((count + 1))
+done
 
 	# Fix eww,  when entering fullscreen state
 	bspc subscribe node_state | while read -r _ _ _ _ state flag; do
